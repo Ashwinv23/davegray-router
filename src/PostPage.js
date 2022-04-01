4900eddbf1e10772ba8225496aca5a19ad009a6e
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-04-01 16:38:55 +0530

    some small changes

--- src/PostPage.js
+++ src/PostPage.js
@@ -14,9 +14,14 @@ const PostPage = ({ posts, handleDelete }) => {
             <p className="postDate">{post.datetime}</p>
             <p className="postBody">{post.body}</p>
             <Link to={`/edit/${id}`}>
-              <button>Edit Post</button>
+              <button className="editBtn">Edit Post</button>
             </Link>
-            <button onClick={(e) => handleDelete(post.id)}>Delete Post</button>
+            <button
+              className="deleteBtn"
+              onClick={(e) => handleDelete(post.id)}
+            >
+              Delete Post
+            </button>
           </>
         )}
         {!post && (
